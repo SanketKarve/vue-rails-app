bd687ac8790d99f4d71fdb7da408eebfa7d94f4d
SanketKarve <sanketkarve43@gmail.com>
2019-01-26 10:28:39 +0530

    fix require params issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
